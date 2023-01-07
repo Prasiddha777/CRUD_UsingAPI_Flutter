d594bb24f19452dc7b78f7d47d4a6d3166c2b16e
Prasiddha Khadka <be2018se630@gces.edu.np>
2023-01-07 16:31:16 +0545

    creating up pages

---
+++ lib/add_todo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddToDoPage extends StatefulWidget {
+  const AddToDoPage({super.key});
+
+  @override
+  State<AddToDoPage> createState() => _AddToDoPageState();
+}
+
+class _AddToDoPageState extends State<AddToDoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add ToDo'),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:crud_api/add_todo.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          navigateToAddPage();
+        },
+        label: Text(
+          'Add Todo',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(builder: (context) => const AddToDoPage());
+    Navigator.push(context, route);
+  }
+}
